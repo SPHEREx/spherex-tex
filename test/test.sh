41ebb2bd56ec29cbe6acf93999b7c941c95525dc
Jonathan Sick <jonathan@jsick.codes>
2023-01-10 22:12:51 -0500

    Add commands for marking up DOORS ID
    - \VADoorsID (with optional URL)
    - \ReqDoorsID (with optional URL)
    - \IPACJiraID (for internal issues; auto links)
    
    The title page includes this information, if set.
    
    The test/ssdc-tr-va and test/ssdc-tr-req documents test this
    functionality.

--- test/test.sh
+++ test/test.sh
@@ -4,3 +4,5 @@ make -C pipeline_module clean && make -C pipeline_module
 make -C pipeline_module_unapproved clean && make -C pipeline_module_unapproved
 make -C technote clean && make -C technote
 make -C ssdc-if clean && make -C ssdc-if
+make -C ssdc-tr-va clean && make -C ssdc-tr-va
+make -C ssdc-tr-req clean && make -C ssdc-tr-req
